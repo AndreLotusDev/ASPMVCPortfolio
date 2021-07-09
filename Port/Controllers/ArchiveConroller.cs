08807e37253c218a4ac6ec225445b9ea16951e81
Andre Soares Gomes <Andre Soares Gomes@DESKTOP-M5UB5AR>
2021-07-08 21:05:31 -0300

    Add project files.

---
+++ Port/Controllers/ArchiveConroller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
+using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Port.Controllers
+{
+    public class ArchivesController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ArchivesController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public FileResult MyCvDownload() 
+        {
+            string path = _webHostEnvironment.ContentRootPath + @"\wwwroot\archives\my_cv.pdf";
+            byte[] cvBytes = System.IO.File.ReadAllBytes(path);
+
+            string cvDownloadName = "cv_download.pdf";
+            return File(cvBytes, System.Net.Mime.MediaTypeNames.Application.Pdf, cvDownloadName);
+        }
+    }
+}
---
+++ Port/Controllers/WorksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Port.Controllers
+{
+    public class WorksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
